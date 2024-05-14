9ee98cd511ee294041449d545803595982e36f7b
Nikolay <koddek@bk.ru>
2024-05-14 08:21:19 +0300

    first commit

---
+++ MovieAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieAPI
+//
+//  Created by KOДИ on 14.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
