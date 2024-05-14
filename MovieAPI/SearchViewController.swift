48e664a9553976d6ef87a5385850d1d776ba66fe
Nikolay <koddek@bk.ru>
2024-05-14 09:50:08 +0300

    setup project

--- MovieAPI/SearchViewController.swift
+++ MovieAPI/SearchViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  SearchViewController.swift
 //  MovieAPI
 //
 //  Created by KOДИ on 14.05.2024.
@@ -7,13 +7,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class SearchViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
     }
-
-
 }
-
